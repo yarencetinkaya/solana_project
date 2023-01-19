ac09a2a5d16e6e3e327ed567581924fbbe168b3b
yaren <cetinkayaa.yarenn@gmail.com>
2023-01-19 15:31:41 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,121 @@
+import React, { useState } from "react";
+import YouTube from "react-youtube";
+import { Layout, Menu, theme, Button, Space } from "antd";
+import { DollarOutlined } from "@ant-design/icons";
+
+const { Header, Content, Footer } = Layout;
+
+const App: React.FC = () => {
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
+
+  const [videoId, setVideoId] = useState(1);
+  const [buttonDisabled, setButtonDisabled] = useState(true);
+  const [videoLink, setVideoLink] = useState("tAGnKpE4NCI");
+  const [showButton, setShowButton] = useState(true);
+  const [claimButton, setClaimButton] = useState(false);
+
+  const handleVideoEnd = () => {
+    setButtonDisabled(false);
+  };
+
+  const changeVideo = () => {
+    switch (videoId) {
+      case 1:
+        setVideoLink("-TSJxAKoMlE");
+        break;
+      case 2:
+        setVideoLink("zDILAUHWg2s");
+        break;
+      case 3:
+        setVideoLink("6XS2A41F5xU");
+        setShowButton(false);
+        setClaimButton(true);
+        break;
+      default:
+        console.log("Error occured while setting video");
+        break;
+    }
+  };
+
+  const handleButtonClick = () => {
+    setButtonDisabled(true);
+    setVideoId(videoId === 3 ? 1 : videoId + 1);
+    changeVideo();
+  };
+
+  const claimSol = () => {
+    console.log("Button clicked");
+  };
+
+  return (
+    <Layout>
+      <Header style={{ position: "sticky", top: 0, zIndex: 1, width: "100%" }}>
+        <div
+          style={{
+            float: "left",
+            width: 120,
+            height: 31,
+            margin: "16px 24px 16px 0",
+            background: "rgba(255, 255, 255, 0.2)",
+          }}
+        />
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={["1"]}
+          items={new Array(3).fill(null).map((_, index) => ({
+            key: String(index + 1),
+            label: `Video ${index + 1}`,
+          }))}
+        />
+      </Header>
+      <Content
+        className="site-layout"
+        style={{ padding: "0 50px", alignSelf: "center" }}
+      >
+        <div
+          style={{
+            padding: 24,
+            minHeight: 380,
+            background: "#0045",
+            margin: 16,
+          }}
+        >
+          <YouTube videoId={videoLink} onEnd={handleVideoEnd} />
+          <Space>
+            <Button
+              type="primary"
+              disabled={buttonDisabled}
+              onClick={handleButtonClick}
+              style={{
+                allign: "center",
+                alignItems: "center",
+                margin: "16px 0",
+                justifyContent: "center",
+                display: showButton ? "block" : "none",
+              }}
+            >
+              Next Video
+            </Button>
+
+            <Button
+              type="primary"
+              icon={<DollarOutlined />}
+              style={{ display: claimButton ? "block" : "none" }}
+              onClick={claimSol}
+            >
+              Claim SOL Token
+            </Button>
+          </Space>
+        </div>
+      </Content>
+      <Footer style={{ textAlign: "center" }}>
+        Yaren Çetinkaya & Göksu Alçınkaya
+      </Footer>
+    </Layout>
+  );
+};
+
+export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <Footer style={{ textAlign: "center" }}>
+      Ant Design ©2023 Created by Ant UED
+    </Footer>
+  );
+}
